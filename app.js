16e890ec8a57282abcb16800841a5f61cd2bf73b
ndoelger <doelgern@gmail.com>
2023-06-06 16:09:03 -0400

    Commit 3: Rought Finish

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-$(() => {});
+$(() => {
+  $block = $(".block");
+  $title = $(".title");
+
+  $title.on("mouseover", function () {
+    $(this).css("opacity", "1");
+  });
+  $title.on("mouseout", function () {
+    $(this).css("opacity", "0");
+  });
+});
