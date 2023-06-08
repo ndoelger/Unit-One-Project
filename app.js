70e5b282db9490a1005d8bce45d26ae6ecc5098f
ndoelger <doelgern@gmail.com>
2023-06-08 14:06:37 -0400

    Rough Final Draft

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 $(() => {
-  $title = $(".test-title");
+  $title = $(".overlay");
 
   $title.on("mouseover", function () {
     $(this).css("opacity", "1");
   });
   $title.on("mouseout", function () {
     $(this).css("opacity", "0");
+  });
+
+  $headers = $("h1, h2");
+  console.log($headers);
+  $headers.on("mouseover", function () {
+    $(this).css("color", "#b6d98d");
+  });
+  $headers.on("mouseout", function () {
+    $(this).css("color", "#dfe6ec");
+  });
+
+  $icons = $("header img");
+  $icons.on("mouseover", function () {
+    $(this).css("opacity", ".6");
+  });
+  $icons.on("mouseout", function () {
+    $(this).css("opacity", "1");
+  });
+
+  $button = $("button");
+  $button.on("mouseover", function () {
+    $(this).css({
+      "background-color": "#b6d98d",
+      cursor: "pointer",
+      color: "black",
+    });
+  });
+  $button.on("mouseout", function () {
+    $(this).css({
+      "background-color": "#586c80",
+      cursor: "pointer",
+      color: "white",
+    });
   });
 });
